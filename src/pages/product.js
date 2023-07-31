9eeb344c8c45c1b327cf585b3838bf680a81ac5a
Your Name <you@example.com>
2023-07-31 22:10:14 +0530

    Message

---
+++ src/pages/product.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function product() {
+    return (
+        <div>
+            <div>
+                <a href="https://www.amazon.in/dp/B0C5DY9W12?&linkCode=li2&tag=twinz05-21&linkId=8ca069c1442046d3e696ba2c16cdf32d&language=en_IN&ref_=as_li_ss_il" target="_blank">
+                    <img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0C5DY9W12&Format=_SL160_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=twinz05-21&language=en_IN" />
+
+                </a>
+                <img src="https://ir-in.amazon-adsystem.com/e/ir?t=twinz05-21&language=en_IN&l=li2&o=31&a=B0C5DY9W12" width="1" height="1" border="0" alt="" />
+            </div>
+
+            <div>
+                <a href="https://www.amazon.in/dp/B0BYDZWSYB?&linkCode=li2&tag=twinz05-21&linkId=24e193a4b2c88936938956f295746581&language=en_IN&ref_=as_li_ss_il" target="_blank">
+                    <img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0BYDZWSYB&Format=_SL160_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=twinz05-21&language=en_IN" />
+                </a>
+                <img src="https://ir-in.amazon-adsystem.com/e/ir?t=twinz05-21&language=en_IN&l=li2&o=31&a=B0BYDZWSYB" width="1" height="1" border="0" alt="" />
+            </div>
+
+            <div>
+                <a href="https://www.amazon.in/Cetaphil-Cleanser-Oily-Skin-125ml/dp/B01CCGW732?keywords=cetaphil+face+wash&qid=1690821406&s=beauty&sprefix=ce%2Cbeauty%2C371&sr=1-6&linkCode=li2&tag=twinz05-21&linkId=e4531889fed2ddf845c07978faac9c41&language=en_IN&ref_=as_li_ss_il" target="_blank">
+                    <img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01CCGW732&Format=_SL160_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=twinz05-21&language=en_IN" />
+                </a>
+                <img src="https://ir-in.amazon-adsystem.com/e/ir?t=twinz05-21&language=en_IN&l=li2&o=31&a=B01CCGW732" width="1" height="1" border="0" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default product
